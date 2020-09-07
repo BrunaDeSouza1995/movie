8dd80b2e3c27a9dece95580495b4b73ca84d0854
Bruna Souza <brunadesouza1995@gmail.com>
2020-09-07 18:01:58 -0300

    TextAppearance implementation

--- app/src/main/java/com/bruna/movie/feature/movie/ui/MovieActivity.kt
+++ app/src/main/java/com/bruna/movie/feature/movie/ui/MovieActivity.kt
@@ -17,8 +17,27 @@ class MovieActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
 
+//        val build = ShapeAppearanceModel().toBuilder()
+////            .setAllCorners(InnerCutCornerTreatment(20F))
+//            .setLeftEdge(CurvedEdgeTreatment(75F))
+//            .setRightEdge(CurvedEdgeTreatment(75F))
+//            .build()
+//
+//
+//        val shapeDrawable = MaterialShapeDrawable()
+//        shapeDrawable.fillColor = ColorStateList.valueOf(
+//            ContextCompat.getColor(
+//                this,
+//                R.color.colorAccent
+//            )
+//        )
+//        shapeDrawable.shapeAppearanceModel = build
+//        card.background = shapeDrawable
+
         viewModel.teste()
 
         viewModel.teste.observe(this) { testText.text = it }
     }
 }
+
+
